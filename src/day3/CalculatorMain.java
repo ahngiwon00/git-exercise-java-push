5c66db9f35f53a496ea17d746e63b2d3a5b8ae0d
ahngiwon00 <ahngiwon00@naver.com>
2022-10-05 10:20:03 +0900

    계산기

---
+++ src/day3/CalculatorMain.java
@@ -0,0 +1,12 @@
+package day3;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.plus(30,10);
+        c.minus(30,10);
+        c.divide(30,10);
+        c.multiple(30,10);
+    }
+
+}
