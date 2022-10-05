47d21fcce597c7bb8e1c92139d12ff09136d53eb
ahngiwon00 <ahngiwon00@naver.com>
2022-10-05 10:27:59 +0900

    나누기 수정

--- src/day3/Calculator.java
+++ src/day3/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
         System.out.println(a*b);
     }
     public void divide(int a,int b){
-        System.out.println(a/b);
+        System.out.println((float) a/b);
     }
 
 
--- src/day3/CalculatorMain.java
+++ src/day3/CalculatorMain.java
@@ -5,7 +5,7 @@ public class CalculatorMain {
         Calculator c = new Calculator();
         c.plus(30,10);
         c.minus(30,10);
-        c.divide(30,10);
+        c.divide(10,20);
         c.multiple(30,10);
     }
 
