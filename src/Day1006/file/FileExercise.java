d6d2fd16933e591665879b1316fb2ca5828de38c
ahngiwon00 <ahngiwon00@naver.com>
2022-10-06 15:15:43 +0900

    루트 파일 목록 출력

--- src/Day1006/file/FileExercise.java
+++ src/Day1006/file/FileExercise.java
@@ -9,8 +9,19 @@ public class FileExercise {
     public static void main(String[] args) throws FileNotFoundException {
         File dir= new File("./");
         File files[] = dir.listFiles();
+        //listFiles() : 디렉토리 경로에 있는 파일들을 배열로 리턴
         for(File file : files){
             System.out.println(file);
         }
     }
 }
+
+/*
+.\.git
+        .\.gitignore
+        .\.idea
+        .\a_file.txt
+        .\java-project-exercise.iml
+        .\out
+        .\src
+*/
