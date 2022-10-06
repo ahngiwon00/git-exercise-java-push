6d289638094be35e9e4a1863190b9d8c47e83090
ahngiwon00 <ahngiwon00@naver.com>
2022-10-06 09:45:31 +0900

    리스트 연습

---
+++ src/Day1006/ListExercise.java
@@ -0,0 +1,17 @@
+package Day1006;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExercise {
+    List<String> students;
+
+    public ListExercise(){
+        this.students = new ArrayList<>();
+        this.students.add("안소민");
+    }
+    public List<String> getStudents(){
+        return students;
+    }
+
+}
