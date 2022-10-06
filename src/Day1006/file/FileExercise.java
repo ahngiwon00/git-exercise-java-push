ccccb41da4469d5f9af375621cdfb70959cfcb85
ahngiwon00 <ahngiwon00@naver.com>
2022-10-06 15:51:39 +0900

    한글자 두글자 n글자 파일 읽기 연습

--- src/Day1006/file/FileExercise.java
+++ src/Day1006/file/FileExercise.java
@@ -3,16 +3,52 @@ package Day1006.file;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class FileExercise {
-
-    public static void main(String[] args) throws FileNotFoundException {
+    public void printFiles(){
         File dir= new File("./"); // ./->현재디렉토리 뜻함
         File files[] = dir.listFiles();
         //listFiles() : 디렉토리 경로에 있는 파일들을 배열로 리턴
         for(File file : files){
             System.out.println(file);
         }
+    }
+
+    public char readAChar(String filename) throws IOException{
+        FileReader fileReader= new FileReader(filename);
+        return (char) fileReader.read();
+    }
+
+    public String read2Chars(String filename) throws IOException{
+        FileReader fileReader= new FileReader(filename);
+        String str= "";
+        str+=(char)fileReader.read();
+        str+=(char)fileReader.read();
+        return str;
+    }
+    public String readNChars(String filename,int num) throws IOException {
+        FileReader fileReader= new FileReader(filename);
+        String str="";
+        for(int i=0; i<num; i++){
+            int asciiCd=fileReader.read();
+            if(asciiCd==-1){
+                return str;
+            }
+            str+=(char)asciiCd;
+        }
+        return str;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        FileExercise fileExercise = new FileExercise();
+        char c = fileExercise.readAChar("a_file.txt");
+        System.out.println(c);
+        String str= fileExercise.read2Chars("a_file.txt");
+        System.out.println(str);
+        String str1= fileExercise.readNChars("a_file.txt",20);
+        System.out.println(str1);
     }
 }
 
