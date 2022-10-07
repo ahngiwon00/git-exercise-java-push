5f337d0af1e84aa345a0bffa85b7d260600c4ce4
ahngiwon00 <ahngiwon00@naver.com>
2022-10-07 16:08:07 +0900

    객체지향 설계 연습

---
+++ src/Day1007/fileSolid/SoutDoSomething.java
@@ -0,0 +1,13 @@
+package Day1007.fileSolid;
+
+import Day1007.bigfile.PopulationMove;
+import Day1007.fileSolid.DoSomething;
+
+import java.util.List;
+
+public class SoutDoSomething implements DoSomething<PopulationMove> {
+    @Override
+    public List<PopulationMove> work(String str) {
+        return null;
+    }
+}
