38063e1efa7b92fb3b970d2deab84fa52ef11c89
ahngiwon00 <ahngiwon00@naver.com>
2022-10-07 14:49:52 +0900

    1.List<PopulationMove>를 순환 하면서 “11,26” 이런 형태의 key를 만든다. 2.moveCntMap에서 해당 key에 해당하는 Object가 없으면 생성하고 1이라고 check합니다. 3.key로 꺼내서 +1 4.리턴 moveCntMap 5.count한 결과를 파일로 저장하기

--- src/Day1007/bigfile/PopulationStatistics.java
+++ src/Day1007/bigfile/PopulationStatistics.java
@@ -5,7 +5,9 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PopulationStatistics {
 
@@ -93,28 +95,51 @@ public class PopulationStatistics {
     public String fromToString(PopulationMove populationMove){
         return populationMove.getFromSido()+","+ populationMove.getToSido()+"\n";
     }
+    public Map<String,Integer> getMoveCntMap(List<PopulationMove> pml){
+        Map<String, Integer> moveCntMap = new HashMap<>();
+        for (PopulationMove pm : pml){
+            String key = pm.getFromSido()+ ","+pm.getToSido();
+            moveCntMap.put(key, moveCntMap.getOrDefault(key,0)+1);
+
+        }
+        return moveCntMap;
+
+    }
 
     public static void main(String[] args) throws IOException {
         //String address = "C:\\Users\\SoMin\\Downloads\\2021_인구관련연간자료_20220927_66125.csv";
         String address = "./from_to.txt";
-        PopulationStatistics populationStatistics=new PopulationStatistics();
-        List<PopulationMove> pml = populationStatistics.readFileByLine(address);
+        PopulationStatistics ps=new PopulationStatistics();
+        List<PopulationMove> pml = ps.readFileByLine(address);
 
 
         /*List<String> strings = new ArrayList<>();
         for(PopulationMove pm : pml){
             //System.out.println("전입:"+pm.getFromSido()+"전출"+pm.getToSido());
-            String fromTo= populationStatistics.fromToString(pm);
+            String fromTo= ps.fromToString(pm);
             strings.add(fromTo);
         }*/
 
-        //populationStatistics.createAFile("from_to.txt");  //파일생성
+        //ps.createAFile("from_to.txt");  //파일생성
 
-        //populationStatistics.write(strings,"./from_to.txt");
+        //ps.write(strings,"./from_to.txt");
 
-        for(PopulationMove pm : pml){
+        /*for(PopulationMove pm : pml){
             System.out.printf("전입:%s, 전출:%s\n",pm.getFromSido(),pm.getToSido());
+        }*/
+
+        Map<String,Integer> map= ps.getMoveCntMap(pml);
+        String targetFilename= "for_heatmap.txt";
+        ps.createAFile(targetFilename);
+        List<String> cntResult = new ArrayList<>();
+        for(String key : map.keySet()){
+            String[] fromto = key.split(",");
+            String s = String.format("[%s, %s, %d]\n",fromto[0],fromto[1],map.get(key));
+            cntResult.add(s);
         }
+        ps.write(cntResult,targetFilename);
+
+
 
 
     }
