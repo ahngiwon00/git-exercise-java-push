a26cd59ef1b127f92f58a72f9191c105570779ba
ahngiwon00 <ahngiwon00@naver.com>
2022-10-07 14:07:31 +0900

    파일 생성 ,from_to.txt에 저장하기,파싱 로직 변경

--- src/Day1007/bigfile/PopulationStatistics.java
+++ src/Day1007/bigfile/PopulationStatistics.java
@@ -1,9 +1,6 @@
 package Day1007.bigfile;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -62,7 +59,8 @@ public class PopulationStatistics {
     public PopulationMove parse(String data){
         String[] datalist;
         datalist=data.split(",");
-        return new PopulationMove(datalist[6],datalist[0]);
+        //return new PopulationMove(datalist[6],datalist[0]);
+        return new PopulationMove(datalist[0],datalist[1]);
     }
 
     public void createAFile(String filename){
@@ -77,22 +75,46 @@ public class PopulationStatistics {
 
     //List<String>을 지정한 파일에 write
     public void write(List<String> strs, String filename){
+        File file = new File(filename);
+
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            for(String str : strs) {
+                writer.write(str);
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
+    public String fromToString(PopulationMove populationMove){
+        return populationMove.getFromSido()+","+ populationMove.getToSido()+"\n";
+    }
+
     public static void main(String[] args) throws IOException {
-        String address = "C:\\Users\\SoMin\\Downloads\\2021_인구관련연간자료_20220927_66125.csv";
+        //String address = "C:\\Users\\SoMin\\Downloads\\2021_인구관련연간자료_20220927_66125.csv";
+        String address = "./from_to.txt";
         PopulationStatistics populationStatistics=new PopulationStatistics();
+        List<PopulationMove> pml = populationStatistics.readFileByLine(address);
 
-        /*List<PopulationMove> pml = populationStatistics.readFileByLine(address);
-        System.out.println(pml.size());
 
+        /*List<String> strings = new ArrayList<>();
         for(PopulationMove pm : pml){
-            System.out.println("전입:"+pm.getFromSido()+"전출"+pm.getToSido());
+            //System.out.println("전입:"+pm.getFromSido()+"전출"+pm.getToSido());
+            String fromTo= populationStatistics.fromToString(pm);
+            strings.add(fromTo);
         }*/
 
-        populationStatistics.createAFile("from_to.txt");
+        //populationStatistics.createAFile("from_to.txt");  //파일생성
 
+        //populationStatistics.write(strings,"./from_to.txt");
+
+        for(PopulationMove pm : pml){
+            System.out.printf("전입:%s, 전출:%s\n",pm.getFromSido(),pm.getToSido());
+        }
 
 
     }
