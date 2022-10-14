f7676c1c7be781af77cde7b94b569570fb3d1305
ahngiwon00 <ahngiwon00@naver.com>
2022-10-14 09:37:03 +0900

    삽입정령

---
+++ src/Day1014/InsertSort.java
@@ -0,0 +1,26 @@
+package Day1014;
+
+public class InsertSort {
+    public int[] insertSort(int[] arr){
+        for(int i =1; i<arr.length; i++){
+            for(int j=i-1; j>=0; j--){
+                if(arr[j]>arr[j+1]){
+                    int tmp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr= new int[]{8,5,6,2,4};
+        InsertSort is= new InsertSort();
+        for(int a : is.insertSort(arr))
+            System.out.println(a);
+
+
+
+    }
+}
