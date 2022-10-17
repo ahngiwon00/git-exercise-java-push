e2321468001e9fc5088842ebad724a0e8f095aff
ahngiwon00 <ahngiwon00@naver.com>
2022-10-17 09:27:53 +0900

    별찍기

--- src/Day1017/RightTriangle.java
+++ src/Day1017/RightTriangle.java
@@ -1,13 +1,19 @@
 package Day1017;
 
 public class RightTriangle {
-    public static void main(String[] args) {
-        int n=10;
+    public void printStar(int n) {
         for(int i =1; i<=n; i++){
             for (int j=0; j<i; j++)
                 System.out.print("*");
             System.out.print("\n");
         }
+    }
+    public static void main(String[] args) {
+        RightTriangle rt = new RightTriangle();
+
+        int n=10;
+        rt.printStar(n);
+
 
     }
 }
