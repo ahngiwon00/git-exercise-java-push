ff41e91cfbc125569d79bc77a80ba11f0552780c
ahngiwon00 <ahngiwon00@naver.com>
2022-10-17 09:30:54 +0900

    어떤 letter를 출력할지 Constructor를 통해 초기화 할수 있게 Refactoring

--- src/Day1017/RightTriangle.java
+++ src/Day1017/RightTriangle.java
@@ -1,19 +1,25 @@
 package Day1017;
 
 public class RightTriangle {
+
+    private char a;
+
+    public RightTriangle(char a) {
+        this.a = a;
+    }
+
     public void printStar(int n) {
         for(int i =1; i<=n; i++){
             for (int j=0; j<i; j++)
-                System.out.print("*");
+                System.out.print(a);
             System.out.print("\n");
         }
     }
     public static void main(String[] args) {
-        RightTriangle rt = new RightTriangle();
+        RightTriangle rt = new RightTriangle('@');
 
         int n=10;
         rt.printStar(n);
-
 
     }
 }
